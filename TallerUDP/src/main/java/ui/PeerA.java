09bb5185d4a4c26f1454ad154d7e5f6edd602745
josemiguelarmas <jarmasplata@gmail.com>
2025-05-08 09:50:19 -0500

    code2

--- TallerUDP/src/main/java/ui/PeerA.java
+++ TallerUDP/src/main/java/ui/PeerA.java
@@ -5,12 +5,12 @@ import util.UDPConnection;
 // PeerA.java
 public class PeerA {
     public static void main(String[] args) {
-        UDPConnection connection = new UDPConnection(5000); // Puerto para recibir
+        UDPConnection connection = new UDPConnection(6000); // Puerto para recibir
         connection.start(); // Inicia hilo de recepción
 
         // Esperar un poco para asegurar que está escuchando
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
 
-        connection.sendDatagram("Hola desde PeerA", "127.0.0.1", 5001); // Enviar a PeerB
+        connection.sendDatagram("Hola desde PeerA", "127.0.0.1", 6001); // Enviar a PeerB
     }
 }
--- TallerUDP/src/main/java/ui/PeerB.java
+++ TallerUDP/src/main/java/ui/PeerB.java
@@ -5,12 +5,12 @@ import util.UDPConnection;
 // PeerB.java
 public class PeerB {
     public static void main(String[] args) {
-        UDPConnection connection = new UDPConnection(5001); // Puerto para recibir
+        UDPConnection connection = new UDPConnection(6001); // Puerto para recibir
         connection.start(); // Inicia hilo de recepción
 
         // Esperar un poco para asegurar que está escuchando
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
 
-        connection.sendDatagram("Hola desde PeerB", "127.0.0.1", 5000); // Enviar a PeerA
+        connection.sendDatagram("Hola desde PeerB", "172.30.188.93", 6000); // Enviar a PeerA
     }
 }
